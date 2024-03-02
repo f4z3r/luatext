773826b46f7140508f42d4ad40d400cf1d82b808
Jakob Beckmann <jakobbeckmann@pm.me>
2024-03-02 06:25:37 +0100

    docs: add function signatures to all functions

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ To get a list of ANSI color codes run:
 ```bash
 curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/e50a28ec54188d2413518788de6c6367ffcea4f7/print256colours.sh | bash
 ```
+
+<!-- https://luarocks.org/enet-1.0-0.rockspec -->
+<!-- https://github.com/luarocks/luarocks/wiki/Rockspec-format -->
--- luatext.lua
+++ luatext.lua
@@ -1,24 +1,37 @@
 local string = require("string")
 
+local ESCAPE = string.char(27)
+
 ---@enum Layer
 local Layer = {
   FG = 38, -- foreground
   BG = 48, -- background
 }
 
+---return the ansi escape string for a color
+---@param layer Layer
+---@param color number
+---@return string
 local function format_color(layer, color)
-  return string.format(string.char(27) .. "[%d;5;%dm", layer, color)
+  return string.format(ESCAPE .. "[%d;5;%dm", layer, color)
 end
 
+---return the reset ansi escape string
+---@return string
 local function reset()
-  return string.char(27) .. "[0m"
+  return ESCAPE .. "[0m"
 end
 
+---@class ColoredString
+---a string to which colors and modifiers can be applied
 local ColoredString = {}
 
+---create a new ColoredString from a string
+---@param str ColoredString|string?
+---@return ColoredString
 function ColoredString:new(str)
   local obj = str
-  if type(str) ~= "table" then
+  if type(obj) ~= "table" then
     str = str or ""
     obj = {
       _data = str,
@@ -30,52 +43,76 @@ function ColoredString:new(str)
   return obj
 end
 
+---set the text of this ColoredString
+---@param str string
+---@return ColoredString
 function ColoredString:text(str)
   self._data = str
   return self
 end
 
+---set the foreground color of this ColoredString
+---@param color number
+---@return ColoredString
 function ColoredString:fg(color)
   self._prefixes.fg = format_color(Layer.FG, color)
   return self
 end
 
+---set the background color of this ColoredString
+---@param color number
+---@return ColoredString
 function ColoredString:bg(color)
   self._prefixes.bg = format_color(Layer.BG, color)
   return self
 end
 
+---set the font of this ColoredString to bold
+---@return ColoredString
 function ColoredString:bold()
-  self._prefixes.bold = string.char(27) .. "[1m"
+  self._prefixes.bold = ESCAPE .. "[1m"
   return self
 end
 
+---set the font of this ColoredString to dim
+---@return ColoredString
 function ColoredString:dim()
-  self._prefixes.dim = string.char(27) .. "[2m"
+  self._prefixes.dim = ESCAPE .. "[2m"
   return self
 end
 
+---set the font of this ColoredString to italic
+---@return ColoredString
 function ColoredString:italic()
-  self._prefixes.italic = string.char(27) .. "[3m"
+  self._prefixes.italic = ESCAPE .. "[3m"
   return self
 end
 
+---set the font of this ColoredString to underlined
+---@return ColoredString
 function ColoredString:underlined()
-  self._prefixes.underlined = string.char(27) .. "[4m"
+  self._prefixes.underlined = ESCAPE .. "[4m"
   return self
 end
 
+---add a substring to this ColoredString. Substrings will inherit the formatting of their parent.
+---@param str string|ColoredString
+---@return ColoredString
 function ColoredString:add_substring(str)
   self._children = self._children or {}
   self._children[#self._children + 1] = ColoredString:new(str)
   return self
 end
 
+---return the children substrings of this ColoredString
+---@return table
 ---@private
 function ColoredString:children()
   return self._children or {}
 end
 
+---return the formating prefix for this ColoredString
+---@return string
 ---@private
 function ColoredString:prefix()
   local prefix = ""
@@ -85,6 +122,8 @@ function ColoredString:prefix()
   return prefix
 end
 
+---render the ColoredString without resetting the colors at the string edges
+---@return string
 ---@private
 function ColoredString:render_no_reset()
   local res = self:prefix()
@@ -97,6 +136,8 @@ function ColoredString:render_no_reset()
   return res
 end
 
+---render the ColoredString, turning it into an escaped string
+---@return string
 function ColoredString:render()
   local res = reset()
   res = res .. self:render_no_reset()
